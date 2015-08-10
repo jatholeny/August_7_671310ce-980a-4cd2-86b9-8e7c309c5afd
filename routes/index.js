c523d884d4fbc596f9e453242c381e137e882ada
Lei Xu <jatholeny@gmail.com>
2015-08-10 00:58:11 -0700

    ajax test

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+
+router.get('/test1',function(req,res){
+  res.status(200).json({message:'haha'});
+});
+
+router.post('/test2',function(req,res){
+  console.log(req.files);
+  res.status(200).json({message:'haha'})
+});
